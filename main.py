343777cba6c1b1b4af89d13fbe1328b19ee6c8f4
AustinYQM <a.r.mcgowan@gmail.com>
2025-03-15 16:32:20 -0500

    write program

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+def main():
+    book_text = get_book_text('./books/frankenstein.txt')
+    print(book_text)
+
+main()
